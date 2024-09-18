d52c3647da5e45e5efce60d79f34685599793e32
Kremlev Leonid <leeonidix@gmail.com>
2024-09-18 14:20:41 +0500

    Task3 Done

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3
+{
+    /*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+    4 -> да
+    -3 -> нет
+    7 -> нет
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a nubmer: ");
+            double number = double.Parse(Console.ReadLine());
+            if (number % 2 == 0) Console.WriteLine($"{number} -> Yes");
+            else Console.WriteLine($"{number} -> No");
+            Console.ReadKey();
+        }
+    }
+}
